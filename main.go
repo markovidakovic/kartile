6030a45c875caea6c0510f56a8448bb9145aee08
Marko Vidaković <markovidakovic29@gmail.com>
2023-05-18 14:13:26 +0200

    -

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	connStr := "postgresql://kartile:your_password@localhost:5432/kartile?schema=public"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
