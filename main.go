4728fc471bea7b8d46bf208d2bf3dc2b5e767ccc
Marko Vidaković <markovidakovic29@gmail.com>
2023-05-29 15:05:27 +0200

    -

--- main.go
+++ main.go
@@ -248,8 +248,13 @@ func enableCorsMiddleware(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set cors headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		// Handle preflight OPTIONS requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		h(w, r)
 	}
 }
