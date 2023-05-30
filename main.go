6e42ea3c6fb568a9107f18fd4aa74e3650127f33
Marko Vidaković <markovidakovic29@gmail.com>
2023-05-30 13:12:37 +0200

    -

--- main.go
+++ main.go
@@ -248,7 +248,7 @@ func enableCorsMiddleware(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set cors headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		// Handle preflight OPTIONS requests
 		if r.Method == http.MethodOptions {
